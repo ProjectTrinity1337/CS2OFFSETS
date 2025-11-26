24939fa20392c395fdae36dc508c194a9fd0e325
ProjectTrinity1337 <Projecttrinity1337@gmail.com>
2025-11-26 13:20:30 +0000

    Update offsets.hpp

--- offsets.hpp
+++ offsets.hpp
@@ -1,5 +1,5 @@
-// Generated using https://github.com/a2x/cs2-dumper
-// 2025-11-25 23:02:31.151839900 UTC
+// FrezzyHook d1mp
+// 2025-11-25 21:50:16.979880700 UTC
 
 #pragma once
 
@@ -30,6 +30,7 @@ namespace cs2_dumper {
         // Module: engine2.dll
         namespace engine2_dll {
             constexpr std::ptrdiff_t dwBuildNumber = 0x5F13E4;
+            constexpr std::ptrdiff_t dwNetworkGameClient = 0x8EB518;
             constexpr std::ptrdiff_t dwNetworkGameClient_clientTickCount = 0x390;
             constexpr std::ptrdiff_t dwNetworkGameClient_deltaTick = 0x23C;
             constexpr std::ptrdiff_t dwNetworkGameClient_isBackgroundMap = 0x2C1467;
